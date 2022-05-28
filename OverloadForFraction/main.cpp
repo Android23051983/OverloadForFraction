18e82962834b4dab5803b518d680fb888984fa22
Andrei <ikar2008@hotmail.com>
2022-05-28 23:17:02 +0300

    mathematical functions

--- OverloadForFraction/main.cpp
+++ OverloadForFraction/main.cpp
@@ -1,109 +1,143 @@
 /*Программа для демонстрации сложения, вычитания и умножения двух дробей*/
 #include <iostream>
+#include<cmath>
 using namespace std;
 
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+
 //Дробь
-struct Drob {
-    int a;
-    int b;
-    Drob(int a, int b);
-    void NOD();
-    void show();
-};
+class fraction {
+   int numerator;
+   int denominator;
 
-//Конструктор
-Drob::Drob(int a, int b) {
-    this->a = a;
-    this->b = b;
-}
-
-//Сокращение дробей
-void Drob::NOD() {
-    int a_a = a;
-    int b_b = b;
-    int buf;
-    while (b_b != 0) {
-        buf = b_b;
-        b_b = a_a % b_b;
-        a_a = buf;
+public:
+    int get_numerator()const
+    {
+        return numerator;
     }
-    a /= a_a;
-    b /= a_a;
-}
 
-//Показать дробь на экране
-void Drob::show() {
-    int buf = a % b;
-    if (!buf) cout << a / b << endl;
-    else cout << a << "/" << b << endl;
+    int get_denominator()const
+    {
+        return denominator;
+    }
 
-}
+    void set_numerator(int numerator)
+    {
+        this->numerator = numerator;
+    }
+    void set_denominator(int denominator)
+    {
+        this->denominator = denominator;
+    }
 
-//Структура с двумя дробями и операциями сложения, умножения, вычитания
-struct addSubMultDrob {
-    struct Drob ob1; //первая дробь
-    struct Drob ob2; //вторая дробь
-    addSubMultDrob(int a, int b, int a1, int b1) : ob1(a, b), ob2(a1, b1) {} //конструктор копирования
-    friend Drob operator+(Drob ob1, Drob ob2); //функция-друг для перегрухки операции сложения
-    friend Drob operator-(Drob ob1, Drob ob2); //функция-друг для перегрухки операции вычитания
-    friend Drob operator*(Drob ob1, Drob ob2); //функция-друг для перегрухки операции умножения
-    void show(); //вывести результат на экран
+    //Наибольший общий делитель
+    //(англ.) Greatest Common Divisor
+    int gcd(long a, long b) {
+        a = abs(a), b = abs(b);
+        while (a != b) {
+            if (a>b)
+            {
+            long temp = a;
+            a = b;
+            b = temp;
+            }
+            b = b - a;
+        }
+        return a;
+    }
+
+
+    //Наименьшее общее кратное
+    // (англ.) least common multiple
+    int lcm(long a, long b) {
+        int max = b;
+        for (int i = max; i > 0; i++)
+        {
+            if ((i % a == 0) && (i % b == 0))
+            {
+            return i;
+            }
+        }
+    }
+
+    fraction& reduce() {
+        int r = gcd(numerator, denominator);
+        numerator /= r;
+        denominator /= r;
+        cout << "Reduce" << endl;
+        return *this;
+    }
+
+    fraction(long numerator = 1, long denominator = 1)
+    {
+        this->numerator = numerator;
+        this->denominator = denominator;
+    }
+
+    ~fraction()
+    {
+        cout << "Distructor: \t\t" << this << endl;
+    }
+
+    //Рабочая 
+    fraction& operator*(const fraction& f) {
+        numerator *= f.numerator;
+        denominator *= f.denominator;
+        cout << "Operator*:\t" << this << endl;
+        return *this;
+    }
+
+    //Рабочая
+    fraction& operator/(const fraction& f) {
+        numerator *= f.denominator;
+        denominator *= f.numerator;
+        cout << "Operator\\:\t" << this << endl;
+        return *this;
+    }
+
+    //Рабочая
+    fraction& operator-(const fraction& f) {
+        int n = numerator * f.denominator - f.numerator * denominator;
+        int d = denominator * f.denominator;
+        numerator = n / gcd(n, d);
+        denominator = d / gcd(n, d);
+        cout << "Operator-:\t" << this << endl;
+        return *this;
+    }
+
+    //Рабочая
+    fraction& operator+(const fraction& f) {
+        long n = numerator * f.denominator + f.numerator * denominator;
+        long d = denominator * f.denominator;
+        numerator = n / gcd(n, d);
+        denominator = d / gcd(n, d);
+        cout << "Operatpr+:\t\t" << this << endl;
+        return *this;
+    }
+
+    void print()
+    {
+        cout << numerator << "/" << denominator << endl;
+    }
+    
 };
 
-//перегрузка сложения
-Drob operator+(Drob ob1, Drob ob2) {
-    Drob temp(0, 0);
-    temp.b = ob1.b * ob2.b;
-    temp.a = (temp.b / ob1.b) * ob1.a + (temp.b / ob2.b) * ob2.a;
-    return temp;
-}
-
-//перегрузка вычитания
-Drob operator-(Drob ob1, Drob ob2) {
-    Drob temp(0, 0);
-    temp.b = ob1.b * ob2.b;
-    temp.a = (temp.b / ob1.b) * ob1.a - (temp.b / ob2.b) * ob2.a;
-    return temp;
-}
-
-//перегрузка умножения
-Drob operator*(Drob ob1, Drob ob2) {
-    Drob temp(0, 0);
-    temp.a = ob1.a * ob2.a;
-    temp.b = ob1.b * ob2.b;
-    return temp;
-}
-
-//вывести дроби, с которыми проводим действия
-void addSubMultDrob::show() {
-    cout << "First drob " << ob1.a << "/" << ob1.b << endl;
-    cout << "Second drob " << ob2.a << "/" << ob2.b << endl;
-    cout << endl;
-}
 
 
-void main() {
-    Drob ob(0, 0);
-    addSubMultDrob ob2(5, 4, 3, 4); //заводим 2 дроби 5/4 и 3/4
 
-    ob2.show();
-
-    cout << "After sum ";
-
-    ob = ob2.ob1 + ob2.ob2; //сложение
-    ob.NOD();
-    ob.show();
-
-    cout << "After sub ";
-
-    ob = ob2.ob1 - ob2.ob2; //вычитание
-    ob.NOD();
-    ob.show();
-
-    cout << "After mul "; //умножение
-
-    ob = ob2.ob1 * ob2.ob2;
-    ob.NOD();
-    ob.show();
-
+void main() 
+{
+    setlocale(LC_ALL, "RUSSIAN");
+    fraction C;
+    fraction A(2, 10);
+    A.print();
+    cout << "------------------------------------------------------\n";
+    fraction B(5, 7);
+    B.print();
+    C = A.operator-(B);
+    C.reduce();
+    C.print();
 }
