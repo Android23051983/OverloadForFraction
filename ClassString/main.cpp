cd2ee39885fb77613520c14cd214965197d3bf5d
Andrei <ikar2008@hotmail.com>
2022-06-19 17:09:54 +0300

    Header inicializatiod done

--- ClassString/main.cpp
+++ ClassString/main.cpp
@@ -32,25 +32,25 @@ public:
 		this->str = str;
 	}*/
 
-	explicit String(int size = 80)
+	explicit String(int size = 80) :size(size), str(new char[size] {})
 	{
-		this->size = size;
-		this->str = new char[size] {};
+		//this->size = size;
+		//this->str = new char[size] {};
 		cout << "DefaultConstructor:\t" << this << endl;
 	}
-	String(const char* str)
+	String(const char* str) :size(strlen(str) + 1), str(new char[size] {})
 	{
-		this->size = strlen(str) + 1;
-		this->str = new char[size]();
+		//this->size = strlen(str) + 1;
+		//this->str = new char[size]();
 		for (int i = 0; i < size; i++)this->str[i] = str[i];
 		cout << "Constructor:\t" << this << endl;
 	}
 
-	String(const String& other)
+	String(const String& other):size(other.size), str(new char[size] {})
 	{
-		this->size = other.size;
+		//this->size = other.size;
 		//Deep copy
-		this->str = new char[size] {};
+		//this->str = new char[size] {};
 		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
 		cout << "CopyComstructor:\t" << this << endl;
 	}
